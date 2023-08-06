7754dfabe512fb583acc769a81a39170b9a41a69
Shaheer Hashim <shaheerhashim10@gmail.com>
2023-08-06 19:10:01 +0500

    Animate Expert section

--- components/ExpertCard.tsx
+++ components/ExpertCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import Button from './Button';
+import { AnimationWrapper } from '@/app/AnimationWrapper';
 
 export type ExpertCardProps = {
   name: string;
@@ -12,22 +13,87 @@ export type ExpertCardProps = {
 function ExpertCard({ name, image, designation, intro }: ExpertCardProps) {
   return (
     <div className="flex w-[792px] h-[502px] bg-[#0D1D30] rounded-[58px]">
-      <div className="ps-[75px] py-[43px]">
-        <Image
-          src={image}
-          className="rounded-[754px]"
-          width={192}
-          height={417}
-          alt={''}
-        />
-      </div>
-
+      <AnimationWrapper
+        motionProps={{
+          initial: {
+            y: 20,
+            opacity: 0,
+          },
+          transition: { type: 'spring', duration: 1.7, delay: 0.35 },
+          whileInView: { y: 0, opacity: 1 },
+          as: 'div',
+        }}
+      >
+        <div className="ps-[75px] py-[43px]">
+          <Image
+            src={image}
+            className="rounded-[754px]"
+            width={192}
+            height={417}
+            alt={''}
+          />
+        </div>
+      </AnimationWrapper>
       <div className="flex flex-col w-[321px] ml-[73px] pt-[65px]">
-        <span className="text-[32px] font-semibold leading-7">{name}</span>
-        <span className="text-[#667999] pt-5 pb-7">{designation}</span>
-        <p className="leading-5">{intro}</p>
+        <div className="text-[32px] font-semibold leading-7">
+          <AnimationWrapper
+            motionProps={{
+              initial: {
+                y: -20,
+                opacity: 0,
+              },
+              transition: { type: 'spring', duration: 1.7, delay: 0.35 },
+              whileInView: { y: 0, opacity: 1 },
+              as: 'span',
+            }}
+          >
+            {name}
+          </AnimationWrapper>
+        </div>
+
+        <div className="text-[#667999] pt-5 pb-7">
+          <AnimationWrapper
+            motionProps={{
+              initial: {
+                y: -20,
+                opacity: 0,
+              },
+              transition: { type: 'spring', duration: 1.7, delay: 0.4 },
+              whileInView: { y: 0, opacity: 1 },
+              as: 'div',
+            }}
+          >
+            {designation}
+          </AnimationWrapper>
+        </div>
+
+        <AnimationWrapper
+          motionProps={{
+            initial: {
+              y: -20,
+              opacity: 0,
+            },
+            transition: { type: 'spring', duration: 1.7, delay: 0.5 },
+            whileInView: { y: 0, opacity: 1 },
+            as: 'div',
+          }}
+        >
+          <p className="leading-5">{intro}</p>
+        </AnimationWrapper>
+
         <Button className="h-[60px] rounded-[105px] bg-[#84D7E9] mt-[63px] text-black text-lg">
-          Get a consultation
+          <AnimationWrapper
+            motionProps={{
+              initial: {
+                opacity: 0,
+              },
+              transition: { type: 'spring', duration: 1.7, delay: 0.1 },
+              whileInView: { opacity: 1 },
+              as: 'span',
+            }}
+          >
+            Get a consultation
+          </AnimationWrapper>
         </Button>
       </div>
     </div>
--- components/ExpertsCarousel.tsx
+++ components/ExpertsCarousel.tsx
@@ -5,6 +5,7 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 import ExpertCard, { ExpertCardProps } from './ExpertCard';
 import classNames from 'classnames';
 import { blimone } from '@/app/fonts';
+import { AnimationWrapper } from '@/app/AnimationWrapper';
 
 type Props = {};
 
@@ -50,7 +51,7 @@ const ExpertsCarousel = (props: Props) => {
           autoplay: true,
           type: 'loop',
           // rewind : true,
-          interval: 1000,
+          interval: 2000,
           pauseOnHover: true,
           arrows: false,
           pagination: true,
@@ -72,31 +73,55 @@ const ExpertsCarousel = (props: Props) => {
 
       <div className="flex items-center">
         <div className="pb-[120px] pr-4">
-          <svg
-            width="41"
-            height="41"
-            className="animate-spin-slow"
-            viewBox="0 0 41 41"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <AnimationWrapper
+            motionProps={{
+              initial: {
+                x: 200,
+                opacity: 0,
+              },
+              transition: { type: 'spring', duration: 1, delay: 0.5 },
+              whileInView: { x: 0, opacity: 1 },
+              as: 'span',
+            }}
           >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M20.8005 40.2716C31.904 40.2914 40.9213 31.3063 40.9411 20.2028C40.9609 9.09927 31.9758 0.0820399 20.8723 0.0622322C9.76875 0.0424245 0.751521 9.02754 0.731713 20.131C0.711905 31.2346 9.69702 40.2518 20.8005 40.2716ZM22.252 18.4547L23.283 8.3647L18.3195 8.35584L19.3651 18.4495L9.74704 15.594L9.02935 20.4284L18.2459 21.2333L12.3052 29.5274L16.8085 32.0058L20.7248 22.8145L25.0135 32.0204L29.3736 29.5578L23.3613 21.2424L32.6819 20.4706L31.9815 15.6337L22.252 18.4547Z"
-              fill="#D9D9D9"
-            />
-          </svg>
+            <svg
+              width="41"
+              height="41"
+              className="animate-spin-slow"
+              viewBox="0 0 41 41"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M20.8005 40.2716C31.904 40.2914 40.9213 31.3063 40.9411 20.2028C40.9609 9.09927 31.9758 0.0820399 20.8723 0.0622322C9.76875 0.0424245 0.751521 9.02754 0.731713 20.131C0.711905 31.2346 9.69702 40.2518 20.8005 40.2716ZM22.252 18.4547L23.283 8.3647L18.3195 8.35584L19.3651 18.4495L9.74704 15.594L9.02935 20.4284L18.2459 21.2333L12.3052 29.5274L16.8085 32.0058L20.7248 22.8145L25.0135 32.0204L29.3736 29.5578L23.3613 21.2424L32.6819 20.4706L31.9815 15.6337L22.252 18.4547Z"
+                fill="#D9D9D9"
+              />
+            </svg>
+          </AnimationWrapper>
         </div>
 
-        <div
-          className={classNames(
-            'text-[65px] leading-[55px] w-[192px] flex flex-col',
-            blimone.className
-          )}
+        <AnimationWrapper
+          motionProps={{
+            initial: {
+              x: 200,
+              opacity: 0,
+            },
+            transition: { type: 'spring', duration: 1, delay: 0.35 },
+            whileInView: { x: 0, opacity: 1 },
+            as: 'span',
+          }}
         >
-          <span>Our experts say</span>
-        </div>
+          <div
+            className={classNames(
+              'text-[65px] leading-[55px] w-[192px] flex flex-col',
+              blimone.className
+            )}
+          >
+            <span>Our experts say</span>
+          </div>
+        </AnimationWrapper>
       </div>
     </div>
   );
